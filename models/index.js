668376e857b4f7289147449e9825ba9e540876c1
Mikayla Bruce <mkla.w.b@gmail.com>
2022-02-12 09:38:43 -0800

    model files started

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const User = require('./User');
+// User has many posts
+
+// Post belongs to user (Post.user_id)
+
+// User have many comments
+
+// Comments belongTo User (comment.user_id)
+
+// Post have many Comments
+
+// Comment belongTo Post (Comment.post_id)
+
+
+module.exports = {User, Project}
