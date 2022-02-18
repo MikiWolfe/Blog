3422fc9163cb303aea0de69388f1b4d1cce0172c
Mikayla Bruce <mkla.w.b@gmail.com>
2022-02-17 22:39:17 -0800

    routes started

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
-    console.log(`Now listening at http://localhost${PORT}`)
+    console.log(`Now listening at http://localhost:${PORT}`)
   );
 });
