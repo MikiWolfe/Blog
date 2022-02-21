cae3505d21c2501a50274b161d17030c0f580c3f
Mikayla Bruce <mkla.w.b@gmail.com>
2022-02-20 19:26:04 -0800

    trying something to get heroku up and running

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
@@ -28,8 +28,8 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
