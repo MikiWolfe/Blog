cf55d49a58c721cd5430c297d55deaf0a540c072
Mikayla Bruce <mkla.w.b@gmail.com>
2022-02-20 19:04:49 -0800

    still trying to make small changes to can be pushed up to heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;;
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
@@ -32,13 +32,13 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
-    console.log(`Now listening at http://localhost:${PORT}`)
+    console.log(`🚀 Now listening at http://localhost:${PORT} 🚀 `)
   );
 });
