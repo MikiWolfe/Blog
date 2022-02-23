d6efa6cc8db8fb295ccd70a4d52e216ec9b0c18c
Mikayla Bruce <mkla.w.b@gmail.com>
2022-02-22 22:41:17 -0800

    still trying to push to heroku

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const path = require("path");
 const express = require("express");
 const session = require("express-session");
-const routes = require("./controllers");
 const exphbs = require("express-handlebars");
+const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 
 const sequelize = require("./config/connection");
+
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
 const sess = {
-  secret: "Super secret secret",
-  cookie: {},
+  secret: process.env.DB_SECRET,  
+  cookie: {
+      maxAge: 86400,
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -23,18 +28,17 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
-
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`🚀 Now listening at http://localhost:${PORT} 🚀 `));
+  app.listen(PORT, () =>
+    console.log(`🚀 Now listening at http://localhost:${PORT} 🚀 `)
+  );
 });
-   
