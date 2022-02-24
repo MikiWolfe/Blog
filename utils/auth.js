23cae93956654c333cd41d875f6da11aa8a330f0
Mikayla Bruce <mkla.w.b@gmail.com>
2022-02-23 19:38:22 -0800

    getting heroku to work

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,9 @@
 const withAuth = (reg, res, next) => {
-    if (!reg.session.logged_id) {
-        res.redirect('/login');
-    } else {
-        next()
-    }
-    };
+  if (!reg.session.logged_id) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
 
-module.exports = withAuth
-
-
+module.exports = withAuth;
