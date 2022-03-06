b4d054df71f2744a57622e6ce0b1bb02fd363bd3
Mikayla Bruce <mkla.w.b@gmail.com>
2022-03-06 14:59:23 -0800

    able to leave comments

---
+++ public/js/onepost.js
@@ -0,0 +1,26 @@
+const res = require("express/lib/response");
+
+const newComment = document.querySelector("#comment");
+
+const addComment = async (event) => {
+  event.preventDefault();
+
+  const post_id = document.querySelector("#blog-title").getAttribute("data-id");
+  const comment_text = document.querySelector("#comment").value.trim();
+
+  if (comment_text) {
+    const response = await fetch("/api/comment", {
+      method: "POST",
+      body: JSON.stringify({
+        post_id,
+        comment_text,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    document.location.reload();
+    console.log(response);
+  }
+};
+newComment.addEventListener("submit", addComment);
